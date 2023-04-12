422eb27455ca67b8cad62f7dafaa559d6a43fbae
mstuart2097 <mstuart@iastate.edu>
2023-04-12 11:02:21 -0500

    Update stuart_stickle_mle.R

--- stuart_stickle_mle.R
+++ stuart_stickle_mle.R
@@ -1,46 +1,45 @@
 library(tidyverse)
 library(tmvtnorm)
+library(bbmle)
 
 dat <- read.csv("stuart_stickle_imp_raw.csv")
-dat2 <- read.csv("stickleback_fossil_vs_extant.csv")
+M <- max(dat$imputation_number)
 
-dat_small <- dat %>% filter(imputation_number == 1)
-
-mvec <- function(x_m,x_f,theta_m,theta_f,kappa){
+minusll <- function(theta_m,theta_f,kappa,sigma,tau){
+  y <- dt$stl
   t <- dt$time
   sex <- model.matrix(~ 0 + gender,dt)
-  mvec <- (sex %*% c(x_f,x_m))*exp(-kappa*t) +
-    (sex %*% c(theta_f,theta_m))*(1 - exp(-kappa*t))
-  return(mvec)
-}
-
-vcovmat <- function(kappa,sigma){
-  t <- dt$time
-  sex <- model.matrix(~ 0 + gender,dt)
-  vars <- diag(sigma^2/(2*kappa) * (1 - exp(-2*kappa*t)))
+  mm <- (sex %*% c(theta_f,theta_m))
+  vars <- diag(sigma^2,nrow=nrow(dt))
   time_diff <- abs(matrix(t,nrow=nrow(dt),ncol=nrow(dt),byrow=TRUE) -
                    matrix(t,nrow=nrow(dt),ncol=nrow(dt)))
-  time_add <- matrix(t,nrow=nrow(dt),ncol=nrow(dt),byrow=TRUE) +
-              matrix(t,nrow=nrow(dt),ncol=nrow(dt))
-  covs <- sigma^2/(2*kappa)*(exp(-2*kappa*time_diff) * exp(-2*kappa*time_add))
-  vcovmat <- vars+covs
-  return(vcovmat)
+  covs <- tau^2/(1 - kappa^2)*kappa^time_diff
+  vv <- vars + covs
+  return(-dmvnorm(y,mean=mm,sigma=vv,log=TRUE) %>% sum)
 }
 
-minusll <- function(x_m,x_f,theta_m,theta_f,kappa,sigma){
-  mm <- mvec(x_m,x_f,theta_m,theta_f,kappa)
-  vv <- vcovmat(kappa,sigma)
-  y <- dt$stl
-  return(-dmvnorm(y,mean=mm,sigma=vv,log=TRUE) %>% sum)
+minus_ll <- function(theta_m,theta_f,kappa_m,kappa_f,sigma_m,sigma_f,tau_m,tau_f){
+
 }
 
 final <- NULL
 
-for(i in 1:5){
-  print(paste0("Running Imputation ",i))
-  dt <- dat %>% filter(imputation_number == i)
+for(m in 1:M){
+  print(paste0("Running Imputation ",m))
+  dt <- dat %>% filter(imputation_number == m)
   test <- mle2(minusll,
-               start=list(x_m=66,x_f=61,theta_m=66,theta_f=61,kappa=0.44,sigma=11))
-  savs <- c(coef(test)[3:4],vcov(test)[3:4,3:4] %>% as.vector)
+               start=list(theta_m=56,theta_f=53,
+                          kappa=0.5,
+                          sigma=9,
+                          tau=5))
+  savs <- c(coef(test)[1:2],vcov(test)[1:2,1:2] %>% as.vector)
   final <- rbind(final,savs)
 }
+
+thetahat <- apply(final[,1:2],2,mean)
+Bm <- apply(final[,3:6],2,mean) %>% matrix(nrow=2)
+Vm <- cov(final[,1:2])
+Tm <- (1 + 1/M)*Bm + Vm
+tstat <- (c(1,-1) %*% thetahat) / sqrt(c(1,-1) %*% Tm %*% c(1,-1))
+p <- 2*pnorm(tstat,lower.tail=FALSE)
+
