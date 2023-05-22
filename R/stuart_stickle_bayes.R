7ebdca7be851a028d3696d3a202e2edaa794aa61
mstuart2097 <mstuart@iastate.edu>
2023-05-21 19:45:42 -0500

    Update Bayes model

--- R/stuart_stickle_bayes.R
+++ R/stuart_stickle_bayes.R
@@ -21,7 +21,6 @@ data{
   vector[N] ds2;
   vector[N] ds3;
   vector[N] lpt;
-  int<lower=0> mds[N];
   int<lower=0> mdf[N];
   int<lower=0> mav[N];
   int<lower=0> maf[N];
@@ -29,11 +28,11 @@ data{
   int<lower=0> mpt[N];
 }
 parameters{
-  real<lower=0> theta_f[16];
-  real<lower=0> theta_m[16];
-  real<lower=-1,upper=1> kappa[16];
+  real<lower=0> theta_f[15];
+  real<lower=0> theta_m[15];
+  real<lower=-1,upper=1> kappa[15];
   real<lower=0> sigma[10];
-  real<lower=0> tau[16];
+  real<lower=0> tau[15];
   real beta[9];
   vector[2*T] u_stl;
   vector[2*T] u_lps;
@@ -45,7 +44,6 @@ parameters{
   vector[2*T] u_ds2;
   vector[2*T] u_ds3;
   vector[2*T] u_lpt;
-  vector[2*T] u_mds;
   vector[2*T] u_mdf;
   vector[2*T] u_mav;
   vector[2*T] u_maf;
@@ -63,7 +61,6 @@ transformed parameters{
   vector[2*T] mu_ds2;
   vector[2*T] mu_ds3;
   vector[2*T] mu_lpt;
-  vector[2*T] mu_mds;
   vector[2*T] mu_mdf;
   vector[2*T] mu_mav;
   vector[2*T] mu_maf;
@@ -90,18 +87,16 @@ transformed parameters{
   mu_ds3[(T+1):(2*T)] = theta_m[9] + u_ds3[(T+1):(2*T)];
   mu_lpt[1:T] = theta_f[10] + u_lpt[1:T];
   mu_lpt[(T+1):(2*T)] = theta_m[10] + u_lpt[(T+1):(2*T)];
-  mu_mds[1:T] = theta_f[11] + u_mds[1:T];
-  mu_mds[(T+1):(2*T)] = theta_m[11] + u_mds[(T+1):(2*T)];
-  mu_mdf[1:T] = theta_f[12] + u_mdf[1:T];
-  mu_mdf[(T+1):(2*T)] = theta_m[12] + u_mdf[(T+1):(2*T)];
-  mu_mav[1:T] = theta_f[13] + u_mav[1:T];
-  mu_mav[(T+1):(2*T)] = theta_m[13] + u_mav[(T+1):(2*T)];
-  mu_maf[1:T] = theta_f[14] + u_maf[1:T];
-  mu_maf[(T+1):(2*T)] = theta_m[14] + u_maf[(T+1):(2*T)];
-  mu_mcf[1:T] = theta_f[15] + u_mcf[1:T];
-  mu_mcf[(T+1):(2*T)] = theta_m[15] + u_mcf[(T+1):(2*T)];
-  mu_mpt[1:T] = theta_f[16] + u_mpt[1:T];
-  mu_mpt[(T+1):(2*T)] = theta_m[16] + u_mpt[(T+1):(2*T)];
+  mu_mdf[1:T] = theta_f[11] + u_mdf[1:T];
+  mu_mdf[(T+1):(2*T)] = theta_m[11] + u_mdf[(T+1):(2*T)];
+  mu_mav[1:T] = theta_f[12] + u_mav[1:T];
+  mu_mav[(T+1):(2*T)] = theta_m[12] + u_mav[(T+1):(2*T)];
+  mu_maf[1:T] = theta_f[13] + u_maf[1:T];
+  mu_maf[(T+1):(2*T)] = theta_m[13] + u_maf[(T+1):(2*T)];
+  mu_mcf[1:T] = theta_f[14] + u_mcf[1:T];
+  mu_mcf[(T+1):(2*T)] = theta_m[14] + u_mcf[(T+1):(2*T)];
+  mu_mpt[1:T] = theta_f[15] + u_mpt[1:T];
+  mu_mpt[(T+1):(2*T)] = theta_m[15] + u_mpt[(T+1):(2*T)];
 }
 model{
   stl ~ normal(X * mu_stl,sigma[1]);
@@ -114,7 +109,6 @@ model{
   ds2 ~ normal(X * mu_ds2 + beta[7] * (stl - X * mu_stl),sigma[8]);
   ds3 ~ normal(X * mu_ds3 + beta[8] * (stl - X * mu_stl),sigma[9]);
   lpt ~ normal(X * mu_lpt + beta[9] * (stl - X * mu_stl),sigma[10]);
-  mds ~ poisson(exp(X * mu_mds));
   mdf ~ poisson(exp(X * mu_mdf));
   mav ~ poisson(exp(X * mu_mav));
   maf ~ poisson(exp(X * mu_maf));
@@ -141,18 +135,16 @@ model{
   u_ds3[(T+2):(2*T)] ~ normal(kappa[9]*u_ds3[(T+1):(2*T-1)],tau[9]);
   u_lpt[2:T] ~ normal(kappa[10]*u_lpt[1:(T-1)],tau[10]);
   u_lpt[(T+2):(2*T)] ~ normal(kappa[10]*u_lpt[(T+1):(2*T-1)],tau[10]);
-  u_mds[2:T] ~ normal(kappa[11]*u_mds[1:(T-1)],tau[11]);
-  u_mds[(T+2):(2*T)] ~ normal(kappa[11]*u_mds[(T+1):(2*T-1)],tau[11]);
-  u_mdf[2:T] ~ normal(kappa[12]*u_mdf[1:(T-1)],tau[12]);
-  u_mdf[(T+2):(2*T)] ~ normal(kappa[12]*u_mdf[(T+1):(2*T-1)],tau[12]);
-  u_mav[2:T] ~ normal(kappa[13]*u_mav[1:(T-1)],tau[13]);
-  u_mav[(T+2):(2*T)] ~ normal(kappa[13]*u_mav[(T+1):(2*T-1)],tau[13]);
-  u_maf[2:T] ~ normal(kappa[14]*u_maf[1:(T-1)],tau[14]);
-  u_maf[(T+2):(2*T)] ~ normal(kappa[14]*u_maf[(T+1):(2*T-1)],tau[14]);
-  u_mcf[2:T] ~ normal(kappa[15]*u_mcf[1:(T-1)],tau[15]);
-  u_mcf[(T+2):(2*T)] ~ normal(kappa[15]*u_mcf[(T+1):(2*T-1)],tau[15]);
-  u_mpt[2:T] ~ normal(kappa[16]*u_mpt[1:(T-1)],tau[16]);
-  u_mpt[(T+2):(2*T)] ~ normal(kappa[16]*u_mpt[(T+1):(2*T-1)],tau[16]);
+  u_mdf[2:T] ~ normal(kappa[11]*u_mdf[1:(T-1)],tau[11]);
+  u_mdf[(T+2):(2*T)] ~ normal(kappa[11]*u_mdf[(T+1):(2*T-1)],tau[11]);
+  u_mav[2:T] ~ normal(kappa[12]*u_mav[1:(T-1)],tau[12]);
+  u_mav[(T+2):(2*T)] ~ normal(kappa[12]*u_mav[(T+1):(2*T-1)],tau[12]);
+  u_maf[2:T] ~ normal(kappa[13]*u_maf[1:(T-1)],tau[13]);
+  u_maf[(T+2):(2*T)] ~ normal(kappa[13]*u_maf[(T+1):(2*T-1)],tau[13]);
+  u_mcf[2:T] ~ normal(kappa[14]*u_mcf[1:(T-1)],tau[14]);
+  u_mcf[(T+2):(2*T)] ~ normal(kappa[14]*u_mcf[(T+1):(2*T-1)],tau[14]);
+  u_mpt[2:T] ~ normal(kappa[15]*u_mpt[1:(T-1)],tau[15]);
+  u_mpt[(T+2):(2*T)] ~ normal(kappa[15]*u_mpt[(T+1):(2*T-1)],tau[15]);
 
   u_stl[{1,T+1}] ~ normal(0,tau[1]/sqrt(1-kappa[1]*kappa[1]));
   u_lps[{1,T+1}] ~ normal(0,tau[2]/sqrt(1-kappa[2]*kappa[2]));
@@ -164,12 +156,11 @@ model{
   u_ds2[{1,T+1}] ~ normal(0,tau[8]/sqrt(1-kappa[8]*kappa[8]));
   u_ds3[{1,T+1}] ~ normal(0,tau[9]/sqrt(1-kappa[9]*kappa[9]));
   u_lpt[{1,T+1}] ~ normal(0,tau[10]/sqrt(1-kappa[10]*kappa[10]));
-  u_mds[{1,T+1}] ~ normal(0,tau[11]/sqrt(1-kappa[11]*kappa[11]));
-  u_mdf[{1,T+1}] ~ normal(0,tau[12]/sqrt(1-kappa[12]*kappa[12]));
-  u_mav[{1,T+1}] ~ normal(0,tau[13]/sqrt(1-kappa[13]*kappa[13]));
-  u_maf[{1,T+1}] ~ normal(0,tau[14]/sqrt(1-kappa[14]*kappa[14]));
-  u_mcf[{1,T+1}] ~ normal(0,tau[15]/sqrt(1-kappa[15]*kappa[15]));
-  u_mpt[{1,T+1}] ~ normal(0,tau[16]/sqrt(1-kappa[16]*kappa[16]));
+  u_mdf[{1,T+1}] ~ normal(0,tau[11]/sqrt(1-kappa[11]*kappa[11]));
+  u_mav[{1,T+1}] ~ normal(0,tau[12]/sqrt(1-kappa[12]*kappa[12]));
+  u_maf[{1,T+1}] ~ normal(0,tau[13]/sqrt(1-kappa[13]*kappa[13]));
+  u_mcf[{1,T+1}] ~ normal(0,tau[14]/sqrt(1-kappa[14]*kappa[14]));
+  u_mpt[{1,T+1}] ~ normal(0,tau[15]/sqrt(1-kappa[15]*kappa[15]));
 
   theta_f ~ normal(0,100);
   theta_m ~ normal(0,100);
@@ -185,7 +176,6 @@ M <- max(dat$imp)
 for (m in 1:M){
   tmp <- dat %>% filter(imp == m)
   stl <- tmp$stl
-  mds <- tmp$mds
   mdf <- tmp$mdf
   maf <- tmp$maf
   lps <- tmp$lps
@@ -207,18 +197,18 @@ for (m in 1:M){
 
   print(paste("Running Imputation",m))
   fit <- sampling(sm,
-                  list(N=N,T=T,X=X,stl=stl,
-                       lpt=lpt,mds=mds,mdf=mdf,
+                  list(N=N,T=T,X=X,
+                       stl=stl,lpt=lpt,mdf=mdf,
                        maf=maf,lps=lps,ect=ect,
-                       tpg=tpg,mav=mav,mcv=mcv,
+                       tpg=tpg,mav=mav,mcf=mcv,
                        mpt=mpt,cle=cle,pmx=pmx,
                        ds1=ds1,ds2=ds2,ds3=ds3),
                   iter=6000,chains=1,warmup=5000,thin=10,
                   pars = c("theta_f","theta_m","kappa","sigma","tau","beta",
                            "mu_stl","mu_lps","mu_ect","mu_tpg",
                            "mu_cle","mu_pmx","mu_ds1","mu_ds2",
-                           "mu_ds3","mu_lpt","mu_mds","mu_mdf",
-                           "mu_mav","mu_maf","mu_mcf","mu_mpt"))
+                           "mu_ds3","mu_lpt","mu_mdf","mu_mav",
+                           "mu_maf","mu_mcf","mu_mpt"))
   if (m == 1){
     samps <- as.data.frame(fit)
   } else {
