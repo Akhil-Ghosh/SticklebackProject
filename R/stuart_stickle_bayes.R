5ac6070978b2bfdf4042016c9909b4dfb91d3e98
mstuart2097 <mstuart@iastate.edu>
2023-06-08 12:23:57 -0500

    Update Stan Code to be more efficient.

--- R/stuart_stickle_bayes.R
+++ R/stuart_stickle_bayes.R
@@ -4,174 +4,84 @@ library(rstan)
 library(ggridges)
 
 dat <- read.csv("Data/stuart_stickle_100imputations_all.csv")
-dat$mds[dat$mds==1.2] <- 1
 dat$group <- factor(paste(dat$gender,letters[dat$time]))
 
-mod <- "
+cont_ind <- "
 data{
   int<lower=0> N;
   int<lower=0> T;
   matrix[N,2*T] X;
-  vector[N] stl;
-  vector[N] lps;
-  vector[N] ect;
-  vector[N] tpg;
-  vector[N] cle;
-  vector[N] pmx;
-  vector[N] ds1;
-  vector[N] ds2;
-  vector[N] ds3;
-  vector[N] lpt;
-  int<lower=0> mdf[N];
-  int<lower=0> mav[N];
-  int<lower=0> maf[N];
-  int<lower=0> mcv[N];
-  int<lower=0> mds[N];
-  int<lower=0> mpt[N];
+  vector[N] y;
 }
 parameters{
-  real<lower=0> theta_f[16];
-  real<lower=0> theta_m[16];
-  real<lower=-1,upper=1> kappa[16];
-  real<lower=0> sigma[10];
-  real<lower=0> tau[16];
-  real beta[11];
-  vector[2*T] u_stl;
-  vector[2*T] u_lps;
-  vector[2*T] u_ect;
-  vector[2*T] u_tpg;
-  vector[2*T] u_cle;
-  vector[2*T] u_pmx;
-  vector[2*T] u_ds1;
-  vector[2*T] u_ds2;
-  vector[2*T] u_ds3;
-  vector[2*T] u_lpt;
-  vector[2*T] u_mdf;
-  vector[2*T] u_mav;
-  vector[2*T] u_maf;
-  vector[2*T] u_mcv;
-  vector[2*T] u_mds;
-  vector[2*T] u_mpt;
+  real<lower=0> theta_f;
+  real<lower=0> theta_m;
+  real<lower=-1,upper=1> kappa;
+  real<lower=0> sigma;
+  real<lower=0> tau;
+  vector[2*T] u;
 }
 transformed parameters{
-  vector[2*T] mu_stl;
-  vector[2*T] mu_lps;
-  vector[2*T] mu_ect;
-  vector[2*T] mu_tpg;
-  vector[2*T] mu_cle;
-  vector[2*T] mu_pmx;
-  vector[2*T] mu_ds1;
-  vector[2*T] mu_ds2;
-  vector[2*T] mu_ds3;
-  vector[2*T] mu_lpt;
-  vector[2*T] mu_mdf;
-  vector[2*T] mu_mav;
-  vector[2*T] mu_maf;
-  vector[2*T] mu_mcv;
-  vector[2*T] mu_mds;
-  vector[2*T] mu_mpt;
-
-  mu_stl[1:T] = theta_f[1] + u_stl[1:T];
-  mu_stl[(T+1):(2*T)] = theta_m[1] + u_stl[(T+1):(2*T)];
-  mu_lps[1:T] = theta_f[2] + u_lps[1:T];
-  mu_lps[(T+1):(2*T)] = theta_m[2] + u_lps[(T+1):(2*T)];
-  mu_ect[1:T] = theta_f[3] + u_ect[1:T];
-  mu_ect[(T+1):(2*T)] = theta_m[3] + u_ect[(T+1):(2*T)];
-  mu_tpg[1:T] = theta_f[4] + u_tpg[1:T];
-  mu_tpg[(T+1):(2*T)] = theta_m[4] + u_tpg[(T+1):(2*T)];
-  mu_cle[1:T] = theta_f[5] + u_cle[1:T];
-  mu_cle[(T+1):(2*T)] = theta_m[5] + u_cle[(T+1):(2*T)];
-  mu_pmx[1:T] = theta_f[6] + u_pmx[1:T];
-  mu_pmx[(T+1):(2*T)] = theta_m[6] + u_pmx[(T+1):(2*T)];
-  mu_ds1[1:T] = theta_f[7] + u_ds1[1:T];
-  mu_ds1[(T+1):(2*T)] = theta_m[7] + u_ds1[(T+1):(2*T)];
-  mu_ds2[1:T] = theta_f[8] + u_ds2[1:T];
-  mu_ds2[(T+1):(2*T)] = theta_m[8] + u_ds2[(T+1):(2*T)];
-  mu_ds3[1:T] = theta_f[9] + u_ds3[1:T];
-  mu_ds3[(T+1):(2*T)] = theta_m[9] + u_ds3[(T+1):(2*T)];
-  mu_lpt[1:T] = theta_f[10] + u_lpt[1:T];
-  mu_lpt[(T+1):(2*T)] = theta_m[10] + u_lpt[(T+1):(2*T)];
-  mu_mdf[1:T] = theta_f[11] + u_mdf[1:T];
-  mu_mdf[(T+1):(2*T)] = theta_m[11] + u_mdf[(T+1):(2*T)];
-  mu_mav[1:T] = theta_f[12] + u_mav[1:T];
-  mu_mav[(T+1):(2*T)] = theta_m[12] + u_mav[(T+1):(2*T)];
-  mu_maf[1:T] = theta_f[13] + u_maf[1:T];
-  mu_maf[(T+1):(2*T)] = theta_m[13] + u_maf[(T+1):(2*T)];
-  mu_mcv[1:T] = theta_f[14] + u_mcv[1:T];
-  mu_mcv[(T+1):(2*T)] = theta_m[14] + u_mcv[(T+1):(2*T)];
-  mu_mds[1:T] = theta_f[15] + u_mds[1:T];
-  mu_mds[(T+1):(2*T)] = theta_m[15] + u_mds[(T+1):(2*T)];
-  mu_mpt[1:T] = theta_f[16] + u_mpt[1:T];
-  mu_mpt[(T+1):(2*T)] = theta_m[16] + u_mpt[(T+1):(2*T)];
+  vector[2*T] mu;
+  mu[1:T] = theta_f + u[1:T];
+  mu[(T+1):(2*T)] = theta_m + u[(T+1):(2*T)];
 }
+
 model{
-  stl ~ normal(X * mu_stl,sigma[1]);
-  lps ~ normal(X * mu_lps + beta[1] * (stl - X * mu_stl),sigma[2]);
-  ect ~ normal(X * mu_ect + beta[2] * (stl - X * mu_stl),sigma[3]);
-  tpg ~ normal(X * mu_tpg + beta[3] * (stl - X * mu_stl),sigma[4]);
-  cle ~ normal(X * mu_cle + beta[4] * (stl - X * mu_stl),sigma[5]);
-  pmx ~ normal(X * mu_pmx + beta[5] * (stl - X * mu_stl),sigma[6]);
-  ds1 ~ normal(X * mu_ds1 + beta[6] * (stl - X * mu_stl),sigma[7]);
-  ds2 ~ normal(X * mu_ds2 + beta[7] * (stl - X * mu_stl),sigma[8]);
-  ds3 ~ normal(X * mu_ds3 + beta[8] * (stl - X * mu_stl),sigma[9]);
-  lpt ~ normal(X * mu_lpt + beta[9] * (stl - X * mu_stl),sigma[10]);
-  mdf ~ poisson(exp(X * mu_mdf));
-  mav ~ poisson(exp(X * mu_mav + beta[10] * (stl - X * mu_stl) - pow(beta[10]*sigma[1],2)/2));
-  maf ~ poisson(exp(X * mu_maf));
-  mcv ~ poisson(exp(X * mu_mcv + beta[11] * (stl - X * mu_stl) - pow(beta[11]*sigma[1],2)/2));
-  mds ~ poisson(exp(X * mu_mds));
-  mpt ~ poisson(exp(X * mu_mpt));
+  // Data Model
+  y ~ normal(X * mu,sigma);
 
-  u_stl[2:T] ~ normal(kappa[1]*u_stl[1:(T-1)],tau[1]);
-  u_stl[(T+2):(2*T)] ~ normal(kappa[1]*u_stl[(T+1):(2*T-1)],tau[1]);
-  u_lps[2:T] ~ normal(kappa[2]*u_lps[1:(T-1)],tau[2]);
-  u_lps[(T+2):(2*T)] ~ normal(kappa[2]*u_lps[(T+1):(2*T-1)],tau[2]);
-  u_ect[2:T] ~ normal(kappa[3]*u_ect[1:(T-1)],tau[3]);
-  u_ect[(T+2):(2*T)] ~ normal(kappa[3]*u_ect[(T+1):(2*T-1)],tau[3]);
-  u_tpg[2:T] ~ normal(kappa[4]*u_tpg[1:(T-1)],tau[4]);
-  u_tpg[(T+2):(2*T)] ~ normal(kappa[4]*u_tpg[(T+1):(2*T-1)],tau[4]);
-  u_cle[2:T] ~ normal(kappa[5]*u_cle[1:(T-1)],tau[5]);
-  u_cle[(T+2):(2*T)] ~ normal(kappa[5]*u_cle[(T+1):(2*T-1)],tau[5]);
-  u_pmx[2:T] ~ normal(kappa[6]*u_pmx[1:(T-1)],tau[6]);
-  u_pmx[(T+2):(2*T)] ~ normal(kappa[6]*u_pmx[(T+1):(2*T-1)],tau[6]);
-  u_ds1[2:T] ~ normal(kappa[7]*u_ds1[1:(T-1)],tau[7]);
-  u_ds1[(T+2):(2*T)] ~ normal(kappa[7]*u_ds1[(T+1):(2*T-1)],tau[7]);
-  u_ds2[2:T] ~ normal(kappa[8]*u_ds2[1:(T-1)],tau[8]);
-  u_ds2[(T+2):(2*T)] ~ normal(kappa[8]*u_ds2[(T+1):(2*T-1)],tau[8]);
-  u_ds3[2:T] ~ normal(kappa[9]*u_ds3[1:(T-1)],tau[9]);
-  u_ds3[(T+2):(2*T)] ~ normal(kappa[9]*u_ds3[(T+1):(2*T-1)],tau[9]);
-  u_lpt[2:T] ~ normal(kappa[10]*u_lpt[1:(T-1)],tau[10]);
-  u_lpt[(T+2):(2*T)] ~ normal(kappa[10]*u_lpt[(T+1):(2*T-1)],tau[10]);
-  u_mdf[2:T] ~ normal(kappa[11]*u_mdf[1:(T-1)],tau[11]);
-  u_mdf[(T+2):(2*T)] ~ normal(kappa[11]*u_mdf[(T+1):(2*T-1)],tau[11]);
-  u_mav[2:T] ~ normal(kappa[12]*u_mav[1:(T-1)],tau[12]);
-  u_mav[(T+2):(2*T)] ~ normal(kappa[12]*u_mav[(T+1):(2*T-1)],tau[12]);
-  u_maf[2:T] ~ normal(kappa[13]*u_maf[1:(T-1)],tau[13]);
-  u_maf[(T+2):(2*T)] ~ normal(kappa[13]*u_maf[(T+1):(2*T-1)],tau[13]);
-  u_mcv[2:T] ~ normal(kappa[14]*u_mcv[1:(T-1)],tau[14]);
-  u_mcv[(T+2):(2*T)] ~ normal(kappa[14]*u_mcv[(T+1):(2*T-1)],tau[14]);
-  u_mds[2:T] ~ normal(kappa[15]*u_mds[1:(T-1)],tau[15]);
-  u_mds[(T+2):(2*T)] ~ normal(kappa[15]*u_mds[(T+1):(2*T-1)],tau[15]);
-  u_mpt[2:T] ~ normal(kappa[16]*u_mpt[1:(T-1)],tau[16]);
-  u_mpt[(T+2):(2*T)] ~ normal(kappa[16]*u_mpt[(T+1):(2*T-1)],tau[16]);
+  // OU Process for Gender and Time Specific Means
+  u[2:T] ~ normal(kappa*u[1:(T-1)],tau);
+  u[(T+2):(2*T)] ~ normal(kappa*u[(T+1):(2*T-1)],tau);
 
-  u_stl[{1,T+1}] ~ normal(0,tau[1]/sqrt(1-kappa[1]*kappa[1]));
-  u_lps[{1,T+1}] ~ normal(0,tau[2]/sqrt(1-kappa[2]*kappa[2]));
-  u_ect[{1,T+1}] ~ normal(0,tau[3]/sqrt(1-kappa[3]*kappa[3]));
-  u_tpg[{1,T+1}] ~ normal(0,tau[4]/sqrt(1-kappa[4]*kappa[4]));
-  u_cle[{1,T+1}] ~ normal(0,tau[5]/sqrt(1-kappa[5]*kappa[5]));
-  u_pmx[{1,T+1}] ~ normal(0,tau[6]/sqrt(1-kappa[6]*kappa[6]));
-  u_ds1[{1,T+1}] ~ normal(0,tau[7]/sqrt(1-kappa[7]*kappa[7]));
-  u_ds2[{1,T+1}] ~ normal(0,tau[8]/sqrt(1-kappa[8]*kappa[8]));
-  u_ds3[{1,T+1}] ~ normal(0,tau[9]/sqrt(1-kappa[9]*kappa[9]));
-  u_lpt[{1,T+1}] ~ normal(0,tau[10]/sqrt(1-kappa[10]*kappa[10]));
-  u_mdf[{1,T+1}] ~ normal(0,tau[11]/sqrt(1-kappa[11]*kappa[11]));
-  u_mav[{1,T+1}] ~ normal(0,tau[12]/sqrt(1-kappa[12]*kappa[12]));
-  u_maf[{1,T+1}] ~ normal(0,tau[13]/sqrt(1-kappa[13]*kappa[13]));
-  u_mcv[{1,T+1}] ~ normal(0,tau[14]/sqrt(1-kappa[14]*kappa[14]));
-  u_mds[{1,T+1}] ~ normal(0,tau[15]/sqrt(1-kappa[15]*kappa[15]));
-  u_mpt[{1,T+1}] ~ normal(0,tau[16]/sqrt(1-kappa[16]*kappa[16]));
+  // Initial Gender and Time Mean Model
+  u[{1,T+1}] ~ normal(0,tau/sqrt(1-kappa*kappa));
 
+  // Priors
+  theta_f ~ normal(0,100);
+  theta_m ~ normal(0,100);
+  kappa ~ normal(0,1);
+  sigma ~ normal(0,10);
+  tau ~ normal(0,10);
+}
+"
+
+cont_dep <- "
+data{
+  int<lower=0> N;
+  int<lower=0> T;
+  matrix[N,2*T] X;
+  vector[N] y;
+  vector[N] stl;
+}
+parameters{
+  real<lower=0> theta_f;
+  real<lower=0> theta_m;
+  real<lower=-1,upper=1> kappa;
+  real<lower=0> sigma;
+  real<lower=0> tau;
+  real beta;
+  vector[2*T] u;
+}
+transformed parameters{
+  vector[2*T] mu;
+  mu[1:T] = theta_f + u[1:T];
+  mu[(T+1):(2*T)] = theta_m + u[(T+1):(2*T)];
+}
+
+model{
+  // Data Model
+  y ~ normal(X * mu + beta * stl,sigma);
+
+  // OU Process for Gender and Time Specific Means
+  u[2:T] ~ normal(kappa*u[1:(T-1)],tau);
+  u[(T+2):(2*T)] ~ normal(kappa*u[(T+1):(2*T-1)],tau);
+
+  // Initial Gender and Time Mean Model
+  u[{1,T+1}] ~ normal(0,tau/sqrt(1-kappa*kappa));
+
+  // Priors
   theta_f ~ normal(0,100);
   theta_m ~ normal(0,100);
   kappa ~ normal(0,1);
@@ -180,8 +90,99 @@ model{
   beta ~ normal(0,5);
 }
 "
-sm <- stan_model(model_code = mod, verbose = TRUE)
+
+disc_ind <- "
+data{
+  int<lower=0> N;
+  int<lower=0> T;
+  matrix[N,2*T] X;
+  int<lower=0>[N] y;
+}
+parameters{
+  real<lower=0> theta_f;
+  real<lower=0> theta_m;
+  real<lower=-1,upper=1> kappa;
+  real<lower=0> tau;
+  vector[2*T] u;
+}
+transformed parameters{
+  vector[2*T] mu;
+  mu[1:T] = theta_f + u[1:T];
+  mu[(T+1):(2*T)] = theta_m + u[(T+1):(2*T)];
+}
+
+model{
+  // Data Model
+  y ~ poisson_log(X * mu);
+
+  // OU Process for Gender and Time Specific Means
+  u[2:T] ~ normal(kappa*u[1:(T-1)],tau);
+  u[(T+2):(2*T)] ~ normal(kappa*u[(T+1):(2*T-1)],tau);
+
+  // Initial Gender and Time Mean Model
+  u[{1,T+1}] ~ normal(0,tau/sqrt(1-kappa*kappa));
+
+  // Priors
+  theta_f ~ normal(0,100);
+  theta_m ~ normal(0,100);
+  kappa ~ normal(0,1);
+  sigma ~ normal(0,10);
+  tau ~ normal(0,10);
+}
+"
+
+disc_dep <- "
+data{
+  int<lower=0> N;
+  int<lower=0> T;
+  matrix[N,2*T] X;
+  int<lower=0>[N] y;
+  vector[N] stl;
+}
+parameters{
+  real<lower=0> theta_f;
+  real<lower=0> theta_m;
+  real<lower=-1,upper=1> kappa;
+  real<lower=0> tau;
+  real beta;
+  vector[2*T] u;
+}
+transformed parameters{
+  vector[2*T] mu;
+  mu[1:T] = theta_f + u[1:T];
+  mu[(T+1):(2*T)] = theta_m + u[(T+1):(2*T)];
+}
+
+model{
+  // Data Model
+  y ~ poisson_log(X * mu + beta * stl);
+
+  // OU Process for Gender and Time Specific Means
+  u[2:T] ~ normal(kappa*u[1:(T-1)],tau);
+  u[(T+2):(2*T)] ~ normal(kappa*u[(T+1):(2*T-1)],tau);
+
+  // Initial Gender and Time Mean Model
+  u[{1,T+1}] ~ normal(0,tau/sqrt(1-kappa*kappa));
+
+  // Priors
+  theta_f ~ normal(0,100);
+  theta_m ~ normal(0,100);
+  kappa ~ normal(0,1);
+  sigma ~ normal(0,10);
+  tau ~ normal(0,10);
+  beta ~ normal(0,5);
+}
+"
+stan_cont_ind <- stan_model(model_code = cont_ind)
+stan_cont_dep <- stan_model(model_code = cont_dep)
+stan_disc_ind <- stan_model(model_code = disc_ind)
+stan_disc_dep <- stan_model(model_code = disc_dep)
 M <- max(dat$imp)
+
+vars <- c("stl","lps","ect","tpg",
+          "cle","pmx","ds1","ds2",
+          "ds3","lpt","mdf","mav",
+          "maf","mcv","mds","mpt")
 
 for (m in 1:M){
   tmp <- dat %>% filter(imp == m)
