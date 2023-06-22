b57ff3f772534848e51215cae6c8c5be06c2f299
mstuart2097 <mstuart@iastate.edu>
2023-06-22 11:12:07 -0500

    Update STAN model

--- R/stuart_stickle_bayes.R
+++ R/stuart_stickle_bayes.R
@@ -6,47 +6,6 @@ library(ggridges)
 dat <- read.csv("Data/stuart_stickle_100imputations_all.csv")
 dat$group <- factor(paste(dat$gender,letters[dat$time]))
 
-cont_ind <- "
-data{
-  int<lower=0> N;
-  int<lower=0> T;
-  matrix[N,2*T] X;
-  vector[N] y;
-}
-parameters{
-  real<lower=0> theta_f;
-  real<lower=0> theta_m;
-  real<lower=-1,upper=1> kappa;
-  real<lower=0> sigma;
-  real<lower=0> tau;
-  vector[2*T] u;
-}
-transformed parameters{
-  vector[2*T] mu;
-  mu[1:T] = theta_f + u[1:T];
-  mu[(T+1):(2*T)] = theta_m + u[(T+1):(2*T)];
-}
-
-model{
-  // Data Model
-  y ~ normal(X * mu,sigma);
-
-  // OU Process for Gender and Time Specific Means
-  u[2:T] ~ normal(kappa*u[1:(T-1)],tau);
-  u[(T+2):(2*T)] ~ normal(kappa*u[(T+1):(2*T-1)],tau);
-
-  // Initial Gender and Time Mean Model
-  u[{1,T+1}] ~ normal(0,tau/sqrt(1-kappa*kappa));
-
-  // Priors
-  theta_f ~ normal(0,100);
-  theta_m ~ normal(0,100);
-  kappa ~ normal(0,1);
-  sigma ~ normal(0,10);
-  tau ~ normal(0,10);
-}
-"
-
 cont_dep <- "
 data{
   int<lower=0> N;
@@ -58,28 +17,41 @@ data{
 parameters{
   real<lower=0> theta_f;
   real<lower=0> theta_m;
+  real<lower=0> theta_f_stl;
+  real<lower=0> theta_m_stl;
   real<lower=-1,upper=1> kappa;
+  real<lower=-1,upper=1> kappa_stl;
   real<lower=0> sigma;
+  real<lower=0> sigma_stl;
   real<lower=0> tau;
+  real<lower=0> tau_stl;
   real beta;
   vector[2*T] u;
+  vector[2*T] u_stl;
 }
 transformed parameters{
   vector[2*T] mu;
   mu[1:T] = theta_f + u[1:T];
   mu[(T+1):(2*T)] = theta_m + u[(T+1):(2*T)];
+  vector[2*T] mu_stl;
+  mu_stl[1:T] = theta_f_stl + u_stl[1:T];
+  mu_stl[(T+1):(2*T)] = theta_m_stl + u_stl[(T+1):(2*T)];
 }
 
 model{
   // Data Model
-  y ~ normal(X * mu + beta * stl,sigma);
+  y ~ normal(X * mu + beta * (stl - X * mu_stl),sigma);
+  stl ~ normal(X * mu_stl,sigma_stl);
 
   // OU Process for Gender and Time Specific Means
   u[2:T] ~ normal(kappa*u[1:(T-1)],tau);
   u[(T+2):(2*T)] ~ normal(kappa*u[(T+1):(2*T-1)],tau);
+  u_stl[2:T] ~ normal(kappa_stl*u_stl[1:(T-1)],tau_stl);
+  u_stl[(T+2):(2*T)] ~ normal(kappa_stl*u_stl[(T+1):(2*T-1)],tau_stl);
 
   // Initial Gender and Time Mean Model
   u[{1,T+1}] ~ normal(0,tau/sqrt(1-kappa*kappa));
+  u_stl[{1,T+1}] ~ normal(0,tau_stl/sqrt(1-kappa_stl*kappa_stl));
 
   // Priors
   theta_f ~ normal(0,100);
@@ -87,6 +59,11 @@ model{
   kappa ~ normal(0,1);
   sigma ~ normal(0,10);
   tau ~ normal(0,10);
+  theta_f_stl ~ normal(0,100);
+  theta_m_stl ~ normal(0,100);
+  kappa_stl ~ normal(0,1);
+  sigma_stl ~ normal(0,10);
+  tau_stl ~ normal(0,10);
   beta ~ normal(0,5);
 }
 "
@@ -126,7 +103,6 @@ model{
   theta_f ~ normal(0,100);
   theta_m ~ normal(0,100);
   kappa ~ normal(0,1);
-  sigma ~ normal(0,10);
   tau ~ normal(0,10);
 }
 "
@@ -142,38 +118,53 @@ data{
 parameters{
   real<lower=0> theta_f;
   real<lower=0> theta_m;
+  real<lower=0> theta_f_stl;
+  real<lower=0> theta_m_stl;
   real<lower=-1,upper=1> kappa;
+  real<lower=-1,upper=1> kappa_stl;
+  real<lower=0> sigma_stl;
   real<lower=0> tau;
+  real<lower=0> tau_stl;
   real beta;
   vector[2*T] u;
+  vector[2*T] u_stl;
 }
 transformed parameters{
   vector[2*T] mu;
   mu[1:T] = theta_f + u[1:T];
   mu[(T+1):(2*T)] = theta_m + u[(T+1):(2*T)];
+  vector[2*T] mu_stl;
+  mu_stl[1:T] = theta_f_stl + u_stl[1:T];
+  mu_stl[(T+1):(2*T)] = theta_m_stl + u_stl[(T+1):(2*T)];
 }
-
 model{
   // Data Model
-  y ~ poisson_log(X * mu + beta * stl);
+  y ~ poisson_log(X * mu + beta * (stl - X * mu_stl) - pow(beta*sigma_stl,2)/2);
+  stl ~ normal(X * mu_stl,sigma_stl);
 
   // OU Process for Gender and Time Specific Means
   u[2:T] ~ normal(kappa*u[1:(T-1)],tau);
   u[(T+2):(2*T)] ~ normal(kappa*u[(T+1):(2*T-1)],tau);
+  u_stl[2:T] ~ normal(kappa_stl*u_stl[1:(T-1)],tau_stl);
+  u_stl[(T+2):(2*T)] ~ normal(kappa_stl*u_stl[(T+1):(2*T-1)],tau_stl);
 
   // Initial Gender and Time Mean Model
   u[{1,T+1}] ~ normal(0,tau/sqrt(1-kappa*kappa));
+  u_stl[{1,T+1}] ~ normal(0,tau_stl/sqrt(1-kappa_stl*kappa_stl));
 
   // Priors
   theta_f ~ normal(0,100);
   theta_m ~ normal(0,100);
   kappa ~ normal(0,1);
-  sigma ~ normal(0,10);
   tau ~ normal(0,10);
+  theta_f_stl ~ normal(0,100);
+  theta_m_stl ~ normal(0,100);
+  kappa_stl ~ normal(0,1);
+  sigma_stl ~ normal(0,10);
+  tau_stl ~ normal(0,10);
   beta ~ normal(0,5);
 }
 "
-stan_cont_ind <- stan_model(model_code = cont_ind)
 stan_cont_dep <- stan_model(model_code = cont_dep)
 stan_disc_ind <- stan_model(model_code = disc_ind)
 stan_disc_dep <- stan_model(model_code = disc_dep)
