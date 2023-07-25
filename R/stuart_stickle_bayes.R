60cda639bb1fabe4731c0de05526c85262294f67
mstuart2097 <mstuart@iastate.edu>
2023-07-25 13:16:57 -0500

    Update STAN code and manuscript (Note: I, Matt, cannot compile the pdf)

--- R/stuart_stickle_bayes.R
+++ R/stuart_stickle_bayes.R
@@ -11,15 +11,13 @@ cont <- "
 data{
   int<lower=0> N;
   int<lower=0> T;
-  matrix[N,2*T] X;
   vector[N] y;
   vector[N] stl;
+  matrix[N,2*T] X;
   vector[T] time;
   vector[T-1] Delta;
-  int<lower=0,upper=1> OU;
-  int<lower=0,upper=1> trend;
-  int<lower=0,upper=1> OU_stl;
-  int<lower=0,upper=1> trend_stl;
+  int<lower=0,upper=3> mod;
+  int<lower=0,upper=3> mod_stl;
 }
 parameters{
   real beta0_f;
@@ -45,24 +43,40 @@ parameters{
 transformed parameters{
   vector[2*T] mu;
   vector[2*T] mu_stl;
+
+  if (mod == 0){
+    mu[1:T] = beta0_f + beta1_f*time + u[1:T];
+    mu[(T+1):(2*T)] = beta0_m + beta1_m*time + u[(T+1):(2*T)];
+  } else if (mod == 1) {
+    mu[1:T] = beta0_f + beta1_f*time;
+    mu[(T+1):(2*T)] = beta0_m + beta1_m*time;
+  } else if (mod == 2){
+    mu[1:T] = beta0_f + u[1:T];
+    mu[(T+1):(2*T)] = beta0_m + u[(T+1):(2*T)];
+  } else {
+    mu[1:T] = beta0_f + 0*time;
+    mu[(T+1):(2*T)] = beta0_m + 0*time;
+  }
+
+  if (mod_stl==0){
+    mu_stl[1:T] = beta0_f + beta1_f*time + u_stl[1:T];
+    mu_stl[(T+1):(2*T)] = beta0_m + beta1_m*time + u_stl[(T+1):(2*T)];
+  } else if (mod_stl == 1) {
+    mu_stl[1:T] = beta0_f + beta1_f*time;
+    mu_stl[(T+1):(2*T)] = beta0_m + beta1_m*time;
+  } else if (mod_stl == 2){
+    mu_stl[1:T] = beta0_f + u_stl[1:T];
+    mu_stl[(T+1):(2*T)] = beta0_m + u_stl[(T+1):(2*T)];
+  } else {
+    mu_stl[1:T] = beta0_f + 0*time;
+    mu_stl[(T+1):(2*T)] = beta0_m + 0*time;
+  }
+
   vector[T-1] OU_mean;
   vector[T-1] OU_var;
   vector[T-1] OU_mean_stl;
   vector[T-1] OU_var_stl;
-  if (trend == 1){
-    mu[1:T] = beta0_f + beta1_f*time + u[1:T];
-    mu[(T+1):(2*T)] = beta0_m + beta1_m*time + u[(T+1):(2*T)];
-  } else {
-    mu[1:T] = beta0_f + u[1:T];
-    mu[(T+1):(2*T)] = beta0_m + u[(T+1):(2*T)];
-  }
-  if (trend_stl == 1){
-    mu_stl[1:T] = beta0_f_stl + beta1_f_stl*time + u_stl[1:T];
-    mu_stl[(T+1):(2*T)] = beta0_m_stl + beta1_m_stl*time + u_stl[(T+1):(2*T)];
-  } else {
-    mu_stl[1:T] = beta0_f_stl + u_stl[1:T];
-    mu_stl[(T+1):(2*T)] = beta0_m_stl + u_stl[(T+1):(2*T)];
-  }
+
   OU_mean = exp(-kappa*Delta);
   OU_var = pow(tau,2)*(1 - exp(-2*kappa*Delta))/(2*kappa);
   OU_mean_stl = exp(-kappa_stl*Delta);
@@ -74,37 +88,19 @@ model{
   y ~ normal(X * mu + gamma * (stl - X * mu_stl),sigma);
   stl ~ normal(X * mu_stl,sigma_stl);
 
-  if (OU == 1){
-    // OU Process for Gender and Time Specific Means
-    u[2:T] ~ normal(OU_mean .* u[1:(T-1)],sqrt(OU_var));
-    u[(T+2):(2*T)] ~ normal(OU_mean .* u[(T+1):(2*T-1)],sqrt(OU_var));
+  // OU Process for Gender and Time Specific Means
+  u[2:T] ~ normal(OU_mean .* u[1:(T-1)],sqrt(OU_var));
+  u[(T+2):(2*T)] ~ normal(OU_mean .* u[(T+1):(2*T-1)],sqrt(OU_var));
 
-    // Initial Gender and Time Mean Model
-    u[{1,T+1}] ~ normal(0,tau_0);
-  } else {
-    // OU Process for Gender and Time Specific Means
-    u[2:T] ~ normal(0,tau);
-    u[(T+2):(2*T)] ~ normal(0,tau);
+  // Initial Gender and Time Mean Model
+  u[{1,T+1}] ~ normal(0,tau_0);
 
-    // Initial Gender and Time Mean Model
-    u[{1,T+1}] ~ normal(0,tau);
-  }
+  // OU Process for Gender and Time Specific Means STL
+  u_stl[2:T] ~ normal(OU_mean_stl .* u_stl[1:(T-1)],sqrt(OU_var_stl));
+  u_stl[(T+2):(2*T)] ~ normal(OU_mean_stl .* u_stl[(T+1):(2*T-1)],sqrt(OU_var_stl));
 
-  if (OU_stl == 1){
-    // OU Process for Gender and Time Specific Means
-    u_stl[2:T] ~ normal(OU_mean_stl .* u_stl[1:(T-1)],sqrt(OU_var_stl));
-    u_stl[(T+2):(2*T)] ~ normal(OU_mean_stl .* u_stl[(T+1):(2*T-1)],sqrt(OU_var_stl));
-
-    // Initial Gender and Time Mean Model
-    u_stl[{1,T+1}] ~ normal(0,tau_0_stl);
-  } else {
-    // OU Process for Gender and Time Specific Means
-    u_stl[2:T] ~ normal(0,tau_stl);
-    u_stl[(T+2):(2*T)] ~ normal(0,tau_stl);
-
-    // Initial Gender and Time Mean Model
-    u_stl[{1,T+1}] ~ normal(0,tau_stl);
-  }
+  // Initial Gender and Time Mean Model STL
+  u_stl[{1,T+1}] ~ normal(0,tau_0_stl);
 
   // Priors
   beta0_f ~ normal(0,100);
@@ -131,15 +127,13 @@ disc <- "
 data{
   int<lower=0> N;
   int<lower=0> T;
-  matrix[N,2*T] X;
-  int<lower=0> y[N];
+  int y[N];
   vector[N] stl;
+  matrix[N,2*T] X;
   vector[T] time;
   vector[T-1] Delta;
-  int<lower=0,upper=1> OU;
-  int<lower=0,upper=1> trend;
-  int<lower=0,upper=1> OU_stl;
-  int<lower=0,upper=1> trend_stl;
+  int<lower=0,upper=3> mod;
+  int<lower=0,upper=3> mod_stl;
   int<lower=0,upper=1> ind;
 }
 parameters{
@@ -165,73 +159,68 @@ parameters{
 transformed parameters{
   vector[2*T] mu;
   vector[2*T] mu_stl;
+
+  if (mod == 0){
+    mu[1:T] = beta0_f + beta1_f*time + u[1:T];
+    mu[(T+1):(2*T)] = beta0_m + beta1_m*time + u[(T+1):(2*T)];
+  } else if (mod == 1) {
+    mu[1:T] = beta0_f + beta1_f*time;
+    mu[(T+1):(2*T)] = beta0_m + beta1_m*time;
+  } else if (mod == 2){
+    mu[1:T] = beta0_f + u[1:T];
+    mu[(T+1):(2*T)] = beta0_m + u[(T+1):(2*T)];
+  } else {
+    mu[1:T] = beta0_f + 0*time;
+    mu[(T+1):(2*T)] = beta0_m + 0*time;
+  }
+
+  if (mod_stl==0){
+    mu_stl[1:T] = beta0_f + beta1_f*time + u_stl[1:T];
+    mu_stl[(T+1):(2*T)] = beta0_m + beta1_m*time + u_stl[(T+1):(2*T)];
+  } else if (mod_stl == 1) {
+    mu_stl[1:T] = beta0_f + beta1_f*time;
+    mu_stl[(T+1):(2*T)] = beta0_m + beta1_m*time;
+  } else if (mod_stl == 2){
+    mu_stl[1:T] = beta0_f + u_stl[1:T];
+    mu_stl[(T+1):(2*T)] = beta0_m + u_stl[(T+1):(2*T)];
+  } else {
+    mu_stl[1:T] = beta0_f + 0*time;
+    mu_stl[(T+1):(2*T)] = beta0_m + 0*time;
+  }
+
   vector[T-1] OU_mean;
   vector[T-1] OU_var;
   vector[T-1] OU_mean_stl;
   vector[T-1] OU_var_stl;
-  if (trend == 1){
-    mu[1:T] = beta0_f + beta1_f*time + u[1:T];
-    mu[(T+1):(2*T)] = beta0_m + beta1_m*time + u[(T+1):(2*T)];
-  } else {
-    mu[1:T] = beta0_f + u[1:T];
-    mu[(T+1):(2*T)] = beta0_m + u[(T+1):(2*T)];
-  }
 
-  if (trend_stl == 1){
-    mu_stl[1:T] = beta0_f_stl + beta1_f_stl*time + u_stl[1:T];
-    mu_stl[(T+1):(2*T)] = beta0_m_stl + beta1_m_stl*time + u_stl[(T+1):(2*T)];
-  } else {
-    mu_stl[1:T] = beta0_f_stl + u_stl[1:T];
-    mu_stl[(T+1):(2*T)] = beta0_m_stl + u_stl[(T+1):(2*T)];
-  }
   OU_mean = exp(-kappa*Delta);
   OU_var = pow(tau,2)*(1 - exp(-2*kappa*Delta))/(2*kappa);
   OU_mean_stl = exp(-kappa_stl*Delta);
   OU_var_stl = pow(tau_stl,2)*(1 - exp(-2*kappa_stl*Delta))/(2*kappa_stl);
-
-  vector[N] lambda = exp(mu);
 }
+
 model{
   // Data Model
-  if (ind == 0){
-    y ~ poisson_log(X * mu + gamma * (stl - X * mu_stl) - pow(gamma*sigma_stl,2)/2);
-  } else {
+  if (ind == 1){
     y ~ poisson_log(X * mu);
+  } else {
+    y ~ poisson_log(X * mu + gamma * (stl - X * mu_stl) - pow(gamma*sigma_stl,2)/2);
   }
   stl ~ normal(X * mu_stl,sigma_stl);
 
   // OU Process for Gender and Time Specific Means
-  if (OU == 1){
-    // OU Process for Gender and Time Specific Means
-    u[2:T] ~ normal(OU_mean .* u[1:(T-1)],sqrt(OU_var));
-    u[(T+2):(2*T)] ~ normal(OU_mean .* u[(T+1):(2*T-1)],sqrt(OU_var));
+  u[2:T] ~ normal(OU_mean .* u[1:(T-1)],sqrt(OU_var));
+  u[(T+2):(2*T)] ~ normal(OU_mean .* u[(T+1):(2*T-1)],sqrt(OU_var));
 
-    // Initial Gender and Time Mean Model
-    u[{1,T+1}] ~ normal(0,tau_0);
-  } else {
-    // OU Process for Gender and Time Specific Means
-    u[2:T] ~ normal(0,tau);
-    u[(T+2):(2*T)] ~ normal(0,tau);
+  // Initial Gender and Time Mean Model
+  u[{1,T+1}] ~ normal(0,tau_0);
 
-    // Initial Gender and Time Mean Model
-    u[{1,T+1}] ~ normal(0,tau);
-  }
+  // OU Process for Gender and Time Specific Means STL
+  u_stl[2:T] ~ normal(OU_mean_stl .* u_stl[1:(T-1)],sqrt(OU_var_stl));
+  u_stl[(T+2):(2*T)] ~ normal(OU_mean_stl .* u_stl[(T+1):(2*T-1)],sqrt(OU_var_stl));
 
-  if (OU_stl == 1){
-    // OU Process for Gender and Time Specific Means
-    u_stl[2:T] ~ normal(OU_mean_stl .* u_stl[1:(T-1)],sqrt(OU_var_stl));
-    u_stl[(T+2):(2*T)] ~ normal(OU_mean_stl .* u_stl[(T+1):(2*T-1)],sqrt(OU_var_stl));
-
-    // Initial Gender and Time Mean Model
-    u_stl[{1,T+1}] ~ normal(0,tau_0_stl);
-  } else {
-    // OU Process for Gender and Time Specific Means
-    u_stl[2:T] ~ normal(0,tau_stl);
-    u_stl[(T+2):(2*T)] ~ normal(0,tau_stl);
-
-    // Initial Gender and Time Mean Model
-    u_stl[{1,T+1}] ~ normal(0,tau_stl);
-  }
+  // Initial Gender and Time Mean Model STL
+  u_stl[{1,T+1}] ~ normal(0,tau_0_stl);
 
   // Priors
   beta0_f ~ normal(0,100);
@@ -260,88 +249,77 @@ vars <- c("lps","ect","tpg",
           "cle","pmx","ds1","ds2",
           "ds3","lpt","mdf","mav",
           "maf","mcv","mds","mpt")
-for (model_num in 0:15){
-  OU <- model_num %/% 8
-  trend <- (model_num %/% 4) %% 2
-  OU_stl <- (model_num %/% 2) %% 2
-  trend_stl <- model_num %% 2
 
-  for (m in 1:M){
-    tmp <- dat %>% filter(imp == m)
-    X <- model.matrix(~0+group,tmp)
+gender <- matrix((dat$gender=="Female")*1,ncol=M)
+stl <- matrix(dat$stl,ncol=M)
+index <- dat$time[1:nrow(gender)]
 
-    N <- nrow(tmp)
-    T <- max(tmp$time)
+N <- nrow(gender)
+T <- max(dat$time)
 
-    time <- times$Inverted.Year/1000
-    Delta <- diff(times$Inverted.Year)/1000
+time <- times$Inverted.Year/1000
+Delta <- diff(times$Inverted.Year)/1000
 
+for (mod in 0:3){
+  for (mod_stl in 0:3){
     for (var in vars){
-      print(paste("Running Imputation",m,"for variable",var,"Iteration",OU,trend,OU_stl,trend_stl))
+      samps <- NULL
+      y <- matrix(dat[,var],ncol=M)
 
-      y <- tmp[,var]
-      stl <- tmp$stl
-
-      if(var == "mav" | var == "mcv"){
-        fit <- sampling(stan_disc,
-                        list(N=N,T=T,X=X,y=y,stl=stl,
-                             time=time,Delta=Delta,
-                             ind=0,OU=OU,trend=trend,
-                             OU_stl=OU_stl,trend_stl=trend_stl),
-                        iter=1500,chains=1,warmup=1000,thin=5,
-                        pars = c("beta0_f","beta0_m",
-                                 "beta1_f","beta1_m",
-                                 "beta0_f_stl","beta0_m_stl",
-                                 "beta1_f_stl","beta1_m_stl",
-                                 "kappa","kappa_stl","sigma_stl",
-                                 "tau","tau_stl","tau_0","tau_0_stl",
-                                 "gamma","lambda","mu_stl"))
-      } else if (substr(var,1,1) == "m") {
-        fit <- sampling(stan_disc,
-                        list(N=N,T=T,X=X,y=y,stl=stl,
-                             time=time,Delta=Delta,
-                             ind=1,OU=OU,trend=trend,
-                             OU_stl=OU_stl,trend_stl=trend_stl),
-                        iter=1500,chains=1,warmup=1000,thin=5,
-                        pars = c("beta0_f","beta0_m",
-                                 "beta1_f","beta1_m",
-                                 "beta0_f_stl","beta0_m_stl",
-                                 "beta1_f_stl","beta1_m_stl",
-                                 "kappa","kappa_stl","sigma_stl",
-                                 "tau","tau_stl","tau_0","tau_0_stl",
-                                 "gamma","lambda","mu_stl"))
-      } else {
-        fit <- sampling(stan_cont,
-                        list(N=N,T=T,X=X,y=y,stl=stl,
-                             time=time,Delta=Delta,
-                             OU=OU,trend=trend,
-                             OU_stl=OU_stl,trend_stl=trend_stl),
-                        iter=1500,chains=1,warmup=1000,thin=5,
-                        pars = c("beta0_f","beta0_m",
-                                 "beta1_f","beta1_m",
-                                 "beta0_f_stl","beta0_m_stl",
-                                 "beta1_f_stl","beta1_m_stl",
-                                 "kappa","kappa_stl","sigma","sigma_stl",
-                                 "tau","tau_stl","tau_0","tau_0_stl",
-                                 "gamma","mu","mu_stl"))
+      for (m in 1:M){
+        print(paste0("Running Model ",mod,"-",mod_stl," for variable ",var," for Iteration ",m))
+        X <- model.matrix(~0+group,dat %>% filter(imp==m))
+        if(var == "mav" | var == "mcv"){
+          fit <- sampling(stan_disc,
+                          list(N=N,T=T,X=X,
+                               y=y[,m],stl=stl[,m],
+                               index=index,time=time,Delta=Delta,
+                               ind=0,mod=mod,mod_stl=mod_stl),
+                          iter=1100,chains=1,warmup=1000,
+                          pars = c("beta0_f","beta0_m",
+                                   "beta1_f","beta1_m",
+                                   "beta0_f_stl","beta0_m_stl",
+                                   "beta1_f_stl","beta1_m_stl",
+                                   "kappa","kappa_stl","sigma_stl",
+                                   "tau","tau_stl","tau_0","tau_0_stl",
+                                   "gamma","mu","mu_stl"))
+          samps <- rbind(samps,as.data.frame(fit))
+        } else if (substr(var,1,1) == "m") {
+          if (mod_stl == 0){
+            fit <- sampling(stan_disc,
+                            list(N=N,T=T,X=X,
+                                 y=y[,m],stl=stl[,m],
+                                 index=index,time=time,Delta=Delta,
+                                 ind=1,mod=mod,mod_stl=mod_stl),
+                            iter=1100,chains=1,warmup=1000,
+                            pars = c("beta0_f","beta0_m",
+                                     "beta1_f","beta1_m",
+                                     "beta0_f_stl","beta0_m_stl",
+                                     "beta1_f_stl","beta1_m_stl",
+                                     "kappa","kappa_stl","sigma_stl",
+                                     "tau","tau_stl","tau_0","tau_0_stl",
+                                     "gamma","mu","mu_stl"))
+            samps <- rbind(samps,as.data.frame(fit))
+          }
+        } else {
+          fit <- sampling(stan_cont,
+                          list(N=N,T=T,X=X,
+                               y=y[,m],stl=stl[,m],
+                               index=index,time=time,Delta=Delta,
+                               mod=mod,mod_stl=mod_stl),
+                          iter=1100,chains=1,warmup=1000,
+                          pars = c("beta0_f","beta0_m",
+                                   "beta1_f","beta1_m",
+                                   "beta0_f_stl","beta0_m_stl",
+                                   "beta1_f_stl","beta1_m_stl",
+                                   "kappa","kappa_stl","sigma","sigma_stl",
+                                   "tau","tau_stl","tau_0","tau_0_stl",
+                                   "gamma","mu","mu_stl"))
+          samps <- rbind(samps,as.data.frame(fit))
+        }
       }
-
-      if (var == "lps"){
-        imp_fit <- as.data.frame(fit)
-        names(imp_fit) <- paste0(names(imp_fit),"_",var)
-      } else {
-        tmp_fit <- as.data.frame(fit)
-        names(tmp_fit) <- paste0(names(tmp_fit),"_",var)
-        imp_fit <- cbind(imp_fit,tmp_fit)
-      }
-    }
-
-    if (m == 1){
-      samps <- imp_fit
-    } else {
-      samps <- rbind(samps,imp_fit)
+      samps_name <- paste0("Figures/",var,"/posterior_samples_",var,"_",mod,mod_stl,".RDS")
+      saveRDS(samps,file=samps_name)
     }
   }
-  samps_name <- paste0("posterior_samples_",OU,trend,OU_stl,trend_stl,".RDS")
-  saveRDS(samps,file=samps_name)
 }
