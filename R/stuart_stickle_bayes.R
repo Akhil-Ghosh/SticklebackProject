259bb7444ff54c37733624fdaa07211efebcd57a
Stuart <mstuart1@luc.edu>
2023-08-27 16:32:28 -0500

    Add zero-inflated model for the variables we previously discussed.

--- R/stuart_stickle_bayes.R
+++ R/stuart_stickle_bayes.R
@@ -86,6 +86,74 @@ model{
 }
 "
 
+cont_zeros <- "
+data{
+  int<lower=0> N;
+  int<lower=0> T;
+  int<lower=0,upper=1> y;
+  vector[N] stl;
+  matrix[N,2*T] X;
+  vector[T] time;
+  vector[T-1] Delta;
+  int<lower=0,upper=3> mod;
+}
+parameters{
+  real beta0_f;
+  real beta0_m;
+  real beta1_f;
+  real beta1_m;
+  real<lower=0> kappa;
+  real<lower=0> tau;
+  real<lower=0> tau_0;
+  real gamma;
+  vector[2*T] u;
+}
+transformed parameters{
+  vector[2*T] mu;
+  if (mod == 0){
+    mu[1:T] = beta0_f + beta1_f*time + u[1:T];
+    mu[(T+1):(2*T)] = beta0_m + beta1_m*time + u[(T+1):(2*T)];
+  } else if (mod == 1) {
+    mu[1:T] = beta0_f + beta1_f*time;
+    mu[(T+1):(2*T)] = beta0_m + beta1_m*time;
+  } else if (mod == 2){
+    mu[1:T] = beta0_f + u[1:T];
+    mu[(T+1):(2*T)] = beta0_m + u[(T+1):(2*T)];
+  } else {
+    mu[1:T] = beta0_f + 0*time;
+    mu[(T+1):(2*T)] = beta0_m + 0*time;
+  }
+
+  vector[T-1] OU_mean;
+  vector[T-1] OU_var;
+
+  OU_mean = exp(-kappa*Delta);
+  OU_var = pow(tau,2)*(1 - exp(-2*kappa*Delta))/(2*kappa);
+}
+
+model{
+  // Data Model
+  y ~ bernoulli_logit(X * mu + gamma * stl);
+
+  // OU Process for Gender and Time Specific Means
+  u[2:T] ~ normal(OU_mean .* u[1:(T-1)],sqrt(OU_var));
+  u[(T+2):(2*T)] ~ normal(OU_mean .* u[(T+1):(2*T-1)],sqrt(OU_var));
+
+  // Initial Gender and Time Mean Model
+  u[{1,T+1}] ~ normal(0,tau_0);
+
+  // Priors
+  beta0_f ~ normal(0,100);
+  beta0_m ~ normal(0,100);
+  beta1_f ~ normal(0,3);
+  beta1_m ~ normal(0,3);
+  kappa ~ normal(0,1);
+  tau ~ normal(0,10);
+  tau_0 ~ normal(0,20);
+  gamma ~ normal(0,5);
+}
+"
+
 disc <- "
 data{
   int<lower=0> N;
@@ -170,6 +238,8 @@ vars <- c("stl","lps","ect","tpg",
           "ds3","lpt","mdf","mav",
           "maf","mcv","mds","mpt")
 
+cont_w_zeros <- c("lps","tpg","ds1","ds2","ds3")
+
 gender <- matrix((dat$gender=="Female")*1,ncol=M)
 stl <- matrix(dat$stl,ncol=M)
 index <- dat$time[1:nrow(gender)]
@@ -183,7 +253,12 @@ Delta <- diff(times$Inverted.Year)/1000
 for (mod in 0:3){
   for (var in vars){
     samps <- NULL
+    samps_prob <- NULL
     y <- matrix(dat[,var],ncol=M)
+
+    if (var %in% cont_w_zeros){
+      delta <- 1*(y == 0)
+    }
 
     for (m in 1:M){
       print(paste0("Running Model ",mod," for variable ",var," for Iteration ",m))
@@ -227,6 +302,31 @@ for (mod in 0:3){
                                  "tau","tau_0",
                                  "gamma","mu"))
         samps <- rbind(samps,as.data.frame(fit))
+      } else if (var %in% cont_w_zeros){
+        fit <- sampling(stan_cont,
+                        list(N=N,T=T,X=X[y[,m] != 0,],
+                             y=y[y[,m] != 0,m],stl=stl[y[,m] != 0,m],
+                             time=time,Delta=Delta,
+                             ind=1,mod=mod),
+                        iter=1500,chains=1,warmup=1000,thin=5,
+                        pars = c("beta0_f","beta0_m",
+                                 "beta1_f","beta1_m",
+                                 "kappa","sigma",
+                                 "tau","tau_0",
+                                 "gamma","mu"))
+        samps <- rbind(samps,as.data.frame(fit))
+        fit_prob <- sampling(stan_cont,
+                            list(N=N,T=T,X=X,
+                                 y=delta,
+                                 time=time,Delta=Delta,
+                                 ind=1,mod=mod),
+                            iter=1500,chains=1,warmup=1000,thin=5,
+                            pars = c("beta0_f","beta0_m",
+                                     "beta1_f","beta1_m",
+                                     "kappa",
+                                     "tau","tau_0",
+                                     "gamma","mu"))
+        samps_prob <- rbind(samps_prob,as.data.frame(fit))
       } else {
         fit <- sampling(stan_cont,
                         list(N=N,T=T,X=X,
@@ -244,5 +344,9 @@ for (mod in 0:3){
     }
     samps_name <- paste0("Figures/",var,"/posterior_samples_",var,"_",mod,".RDS")
     saveRDS(samps,file=samps_name)
+    if(var %in% cont_w_zeros){
+      samps_prob_name <- paste0("Figures/",var,"/posterior_samples_",var,"prob_",mod,".RDS")
+      saveRDS(samps_prob,file=samps_prob_name)
+    }
   }
 }
